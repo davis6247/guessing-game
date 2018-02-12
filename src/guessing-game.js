643fce09a2dce079328a3e6b7ccf7a46748200e8
davis009 <davis6247@gmail.com>
2018-02-12 17:19:07 +0300

    soved

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
+
 class GuessingGame {
-    constructor() {}
+    
+    constructor() {
+        this.guessedNumber = 0;
+        this.min = 0;
+        this.max = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.guessedNumber  = Math.ceil(((this.max - this.min) / 2) + this.min);;
     }
 
     lower() {
-
+        this.max = this.guessedNumber;
     }
 
     greater() {
-
+        this.min = this.guessedNumber;
     }
 }
 
